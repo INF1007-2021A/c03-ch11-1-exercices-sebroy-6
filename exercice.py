239d3b16204665b7e27dd55225352aa1287a217b
sebroy-6 <sebastien-6.roy@polymtl.ca>
2021-11-16 08:16:10 -0500

    Completed version

--- exercice.py
+++ exercice.py
@@ -8,7 +8,6 @@ from inspect import *
 
 from game import *
 
-
 def main():
 	c1 = Character("Äpik", 200, 150, 70, 70)
 	c2 = Character("Gämmor", 250, 100, 120, 60)
@@ -16,8 +15,10 @@ def main():
 	c1.weapon = Weapon("BFG", 100, 69)
 	c2.weapon = Weapon("Deku Stick", 120, 1)
 
-	turns = run_battle(c1, c2)
-	print(f"The battle ended in {turns} turns.")
+	turns, victor, loser = run_battle(c1, c2)
+	finish = random.choice(["finished", "decapitated", "annihilated", "man handled", "slaughtered"])
+
+	print(f"The victor is {victor.name} who {finish} {loser.name} in {turns} turns!")
 
 if __name__ == "__main__":
 	main()
--- game.py
+++ game.py
@@ -9,7 +9,7 @@ import random
 
 import utils
 
-
+UNARMED_POWER = 20
 class Weapon:
 	"""
 	Une arme dans le jeu.
@@ -18,8 +18,11 @@ class Weapon:
 	:param power: Le niveau d'attaque
 	:param min_level: Le niveau minimal pour l'utiliser
 	"""
+	def __init__(self, name="Unarmed", power=UNARMED_POWER, min_level=0):
 
-	UNARMED_POWER = 20
+		self.name = name
+		self.power = power
+		self.min_level = min_level
 
 
 class Character:
@@ -32,23 +35,56 @@ class Character:
 	:param defense: Le niveau de défense du personnage
 	:param level: Le niveau d'expérience du personnage
 	"""
+	def __init__(self, name="No name", max_hp=100, attack=1, defense=1, level=1):
+		self.name = name
+		self.max_hp = max_hp
+		self.attack = attack
+		self.defense = defense
+		self.level = level
+		self.hp = self.max_hp
+		self.weapon = Weapon()
 
+def compute_damage(attacker, defender):
+	damage_equation = (((((2/5)*attacker.level)+2)*attacker.weapon.power*(attacker.attack/defender.defense)/50) + 2)
+	crit = 2 if random.randint(1, 16) == 1 else 1
+	modifier = crit * (random.randint(85, 100)/100)
+
+	return crit, damage_equation * modifier
 
 
 def deal_damage(attacker, defender):
 	# TODO: Calculer dégâts
 	print(f"{attacker.name} used {attacker.weapon.name}")
-	if crit:
+
+	crit, damage = compute_damage(attacker, defender)
+
+	if crit == 2:
 		print("  Critical hit!")
 	print(f"  {defender.name} took {damage} dmg")
 
+	defender.hp -= damage
+
+	return defender
+
 def run_battle(c1, c2):
 	# TODO: Initialiser attaquant/défendeur, tour, etc.
+	attacker = c1
+	defender = c2
+	turn = 1
+
 	print(f"{attacker.name} starts a battle with {defender.name}!")
+
 	while True:
-		# TODO: Appliquer l'attaque
-		# TODO: Si le défendeur est mort
+		defender = deal_damage(attacker, defender)
+
+		if defender.hp <= 0:
 			print(f"{defender.name } is sleeping with the fishes.")
 			break
-		# Échanger attaquant/défendeur
-	# TODO: Retourner nombre de tours effectués
+
+		swap = attacker
+		attacker = defender
+		defender = swap
+		turn += 1
+
+	return turn, attacker, defender
+
